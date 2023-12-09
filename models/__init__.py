70ef17d52c627af1762fe9d30f9eb7b68286e493
Joe Egboka <joeegboka@gmail.com>
2023-12-09 22:43:38 +0100

    add the models package with all class models that inherit from the base model

---
+++ models/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""__init__ package module"""
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.engine.file_storage import FileStorage
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+storage = FileStorage()
+storage.reload()
+
+all_class = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review,
+}
