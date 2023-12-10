c3a4e0de69344e6fd2362062b653b782be7a58ee
Joe Egboka <joeegboka@gmail.com>
2023-12-10 01:02:59 +0100

    modify base_model module

--- models/base_model.py
+++ models/base_model.py
@@ -29,12 +29,16 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """
+        updates the public instance attribute updated_at with the
+        current datetime
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance
+        """returns a dictionary containing all keys/values of __dict__ of
+        the instance
 
         Returns:
             dict: all keys/values of __dict__ of the instance
